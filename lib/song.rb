29f8d315215854a3b116e2c061e62613639ab3e7
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-05 19:06:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,22 @@
 class Song
   attr_accessor :name, :artist
-
+ @@all = []
   def initialize(name)
     @name = name
+    @songs = []
+    save
   end
 
   def self.all
     @@all
   end
+  def save
+    @@all << self
+  end
 
+  def songs
+    @songs
+  end
     def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
