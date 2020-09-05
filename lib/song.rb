9d55f4d86a208f95d252dd3b77d01c0af9cd1b7f
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-05 19:03:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
     def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
+    song = self.new(song_name)
     song.artist_name = artist_name
     song
   end
